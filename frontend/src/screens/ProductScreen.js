26bded23e9520ba4e5444c6bc6f77d1a465422e4
Damir Kolobaric <dkolobaric@gmail.com>
2022-05-03 18:09:01 +0200

    Implementing React Router
    Implementing React Router

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductScreen = (props) => {
+  return <div>ProductScreen</div>
+}
+
+ProductScreen.propTypes = {}
+
+export default ProductScreen
